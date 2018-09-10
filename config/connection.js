ea92dd1e423f137bfc3b490be8337672c2758a84
llama lord <chasejrabon@gmail.com>
2018-09-09 23:45:50 -0500

    attempting to resolve issues

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,7 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: "localhost",
+        port: 8889,
         user: "root",
         password: "",
         database: "burgers_db"
